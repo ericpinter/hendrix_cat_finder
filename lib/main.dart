c74826f328a0ac99d67b5d4ea5c204a5d17d001a
ZacharyNelson <nelsonza@hendrix.edu>
2020-09-26 22:11:33 -0500

    Created basic cat drop down
    Basic cat drop down

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(
+      title: "Hendrix Cat Finder",
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hendrix Cat Finder',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Hendrix Cat Finder'),
-    );
+class _HomeState extends State<Home> {
+  //IS there a list of cat names somewhere????
+  List<CatNameList> _dropdownlistofcats = [
+    CatNameList(1, "Cat 1"),
+    CatNameList(2, "cat 2"),
+    CatNameList(3, "cat 3"),
+    CatNameList(4, "cat 4")
+  ];
+
+  List<DropdownMenuItem<CatNameList>> _dropdownMenuOfCats;
+  CatNameList _selectedCat;
+
+  void initState() {
+    super.initState();
+    _dropdownMenuOfCats = buildDropDownMenuItems(_dropdownlistofcats);
+    _selectedCat = _dropdownMenuOfCats[0].value;
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  List<DropdownMenuItem<CatNameList>> buildDropDownMenuItems(List CatNames) {
+    List<DropdownMenuItem<CatNameList>> items = List();
+    for (CatNameList Cat in CatNames) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(Cat.name),
+          value: Cat,
+        ),
+      );
+    }
+    return items;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Hendrix Cat Finder"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.red, width: 5.0),
+              ),
+              child: DropdownButton(
+                  value: _selectedCat,
+                  items: _dropdownMenuOfCats,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedCat = value;
+                    });
+                  }),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+          Text(" ${_selectedCat.name}"),
+        ],
       ),
     );
   }
+}
+
+class CatNameList {
+  int value;
+  String name;
+
+  CatNameList(this.value, this.name);
 }
